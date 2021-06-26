248c06eb7bb91106e5c1ab0b4fb9256270a8b714
jose <jose.contreras2020@twk.cl>
2021-06-26 03:48:30 +0200

    Mi primer commit

---
+++ Python/ejercicio_python1.py
@@ -0,0 +1,18 @@
+from os import system
+
+
+system("cls")
+
+#las variables se crean cuando necesitamos ocuparlas
+#el tipo de variable son definidas por su contenido
+
+# viva el heavy metal
+print ("manowar")
+print (666)
+# imprimir 2 cosas distintas
+print ("manowar",666)
+
+from os import system
+
+
+system("cls")
---
+++ Python/ejercicios_jose.py
@@ -0,0 +1,24 @@
+#comparare 2 numeros en una sentencia if
+a = 1
+b = 2
+if a == b :
+    print ("los numeros son iguales")
+else:
+    print ("los numeros son distintos")
+    
+print ("ese es el resultado")
+
+# mi ciclo while
+a = 0
+#mientras a sea menor a 10
+while a<20:
+    a=a+1
+    print(a)
+    
+# mi ciclo for
+# para que me imprimiera 10 numeros lo ise en el rango de 1 a 11
+for a in range(1,11):
+    print(a)
+
+
+
