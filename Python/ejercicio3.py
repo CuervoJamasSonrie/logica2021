f6d7bee674caacfd7d8bd5e06d20215d205278de
jose <jose.contreras2020@twk.cl>
2021-07-02 02:18:49 +0200

    ejercicio4

---
+++ Python/ejercicio3.py
@@ -0,0 +1,18 @@
+#ejercicio
+
+mensaje = """viva el 
+heavy
+metall
+"""
+print (mensaje)
+
+mensaje2 = "bueno \ malo \ feo"
+print (mensaje2)
+#ejercio 4
+numero1=5
+numero2=7
+
+resultado=numero1+numero2
+print(resultado)
+
+lista = [1234,'aeiou']
